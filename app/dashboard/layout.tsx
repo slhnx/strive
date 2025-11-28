f172eca3248c8c7510f0b5a745e84b19327e8763
shaanalam <shaanalam369@gmail.com>
2025-11-29 02:38:14 +0530

    feat:topbar component

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,11 @@
+import Topbar from "@/components/dashboard/topbar";
+
+const DashboardLayout = () => {
+  return (
+    <main className='w-[70%] mx-auto py-12'>
+      <Topbar />
+    </main>
+  );
+};
+
+export default DashboardLayout;
